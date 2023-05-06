eb2ba4ff63ccbdcd1adab49948a1b2c9e591c3e8
SanthoshSivan46 <98525709+SanthoshSivan46@users.noreply.github.com>
2023-05-06 20:42:48 +0530

    Add files via upload

---
+++ Faculty-Assistance-main/source/GPT_3.py
@@ -0,0 +1,44 @@
+import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def generate_response3(prompt):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.2,
+    max_tokens=3000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+    return response["choices"][0]["text"]
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=4000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+        
+    )
+    return response["choices"][0]["text"]
+
+
+def summarizing(pdf_content):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=pdf_content,
+    temperature=0.5,
+    max_tokens=3000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=1
+    )
+    return response["choices"][0]["text"]
+
